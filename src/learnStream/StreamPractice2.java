bc72ab6f4fc81b7a7aeeef2d1aa14dc2201e0ced
aparajit0511 <aparajit35@gmail.com>
2024-06-16 19:51:22 +0530

    more pratice problems

--- src/learnStream/StreamPractice2.java
+++ src/learnStream/StreamPractice2.java
@@ -1,26 +1,24 @@
 package learnStream;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamPractice2 {
     public static void main(String[] args) {
 
         // 7) From the given list of integers, print the numbers which are multiples of 5?
 
-        List<Integer> array1 = Arrays.asList(1,2,5,50,123,12,75,10,20);
+        List<Integer> array1 = Arrays.asList(1, 2, 5, 50, 123, 12, 75, 10, 20);
 
-        for(Integer number:array1){
-            if(number % 5 == 0){
+        for (Integer number : array1) {
+            if (number % 5 == 0) {
                 System.out.println(number + " is a multiple of 5");
             }
         }
 
-        List<Integer> stream_numbers = array1.stream().filter(x->x%5==0).collect(Collectors.toList());
+        List<Integer> stream_numbers = array1.stream().filter(x -> x % 5 == 0).collect(Collectors.toList());
         System.out.println("Multiples of 5 are ->" + stream_numbers);
 
         // 8) Given a list of integers, find maximum and minimum of those numbers?
@@ -29,14 +27,73 @@ public class StreamPractice2 {
         System.out.println("Minimum ->" + array1.stream().min(Comparator.naturalOrder()).get());
 
         // 9) How do you merge two unsorted arrays into single sorted array using Java 8 streams?
-//        List<Integer> array2 = Arrays.asList(11,22,5,50,13,121,75,100,220,1);
+
         int[] array12 = {5, 2, 8, 3};
         int[] array2 = {7, 6, 4, 1};
 
 //        IntStream.concat(Arrays.stream(array1),Arrays.stream(array2));
 
         System.out.println(Arrays.toString(IntStream.concat(Arrays.stream(array12), Arrays.stream(array2)).sorted().toArray()));
-    }
 
-}
+        // 11) How do you get three maximum numbers and three minimum numbers from the given list of integers?
+        List<Integer> array3 = Arrays.asList(11, 22, 5, 50, 13, 121, 75, 100, 220, 1);
+
+        array3.stream().sorted().limit(3).forEach(System.out::println);
+
+        array3.stream().sorted(Comparator.reverseOrder()).limit(3).forEach(System.out::println);
+
+        // 12) Java 8 program to check if two strings are anagrams or not?
+
+        // traditional method
+
+        String str1 = "ana";
+        String str2 = "naa";
+
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+
+        for (int j = 0; j < str1.length(); j++) {
+            char c = str1.charAt(j);
+            if (map1.containsKey(c)) {
+                map1.put(c, map1.get(c) + 1);
+            } else {
+                map1.put(c, 1);
+            }
+        }
+
+
+        System.out.println("Hashmap map1 " + map1);
+
+
+            for (int i = 0; i < str2.length(); i++) {
+                char d = str2.charAt(i);
+                if (map2.containsKey(d)) {
+                    map2.put(d, map2.get(d) + 1);
+                } else {
+                    map2.put(d, 1);
+                }
+
+            }
+        System.out.println("Hashmap map2  " + map2);
+
+            if(map1.equals(map2)){
+                System.out.println("Strings are anagram");
+            }else{
+                System.out.println("Strings are not anagram");
+            }
+
+            // Stream method
+
+        String s1 = Stream.of(str1.split("")).map(String::toLowerCase).sorted().collect(Collectors.joining());
+        String s2 = Stream.of(str2.split("")).map(String::toLowerCase).sorted().collect(Collectors.joining());
+
+        if(s1.equals(s2)){
+            System.out.println("Strings are anagram");
+        }else{
+            System.out.println("Strings are not anagram");
+        }
+
+
+    } // main
+} // class
 
